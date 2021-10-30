831c795369e10b58855972f94d7ce55e9226bed3
simon-lc <simonlc@stanford.edu>
2021-10-30 09:20:19 -0700

    importing tests

--- test/jacobian_test.jl
+++ test/jacobian_test.jl
@@ -1,10 +1,10 @@
-function test_solmat(model::Symbol; ϵ::T = 1e-6, H::T = 0.10, Δt::T = 0.01, g::T = -9.81, verbose::Bool = false, kwargs...) where {T}
+function test_solmat(model::Symbol; ϵ::T = 1e-6, tsim::T = 0.10, Δt::T = 0.01, g::T = -9.81, verbose::Bool = false, kwargs...) where {T}
 
     @testset "solmat: $(string(model))" begin
         mechanism = getmechanism(model, Δt = Δt, g = g, kwargs...)
         initialize!(mechanism, model)
 
-        storage = simulate!(mechanism, H, record = true, solver = :mehrotra!, verbose = false)
+        storage = simulate!(mechanism, tsim, record = true, solver = :mehrotra!, verbose = false)
 
         # Set data
         Nb = length(mechanism.bodies)
@@ -22,12 +22,12 @@ function test_solmat(model::Symbol; ϵ::T = 1e-6, H::T = 0.10, Δt::T = 0.01, g:
     return nothing
 end
 
-function test_datamat(model::Symbol; ϵ::T = 1e-6, H::T = 0.10, Δt::T = 0.01, g::T = -9.81, verbose::Bool = false, kwargs...) where {T}
+function test_datamat(model::Symbol; ϵ::T = 1e-6, tsim::T = 0.10, Δt::T = 0.01, g::T = -9.81, verbose::Bool = false, kwargs...) where {T}
 
     @testset "datamat: $(string(model))" begin
         mechanism = getmechanism(model, Δt = Δt, g = g, kwargs...)
         initialize!(mechanism, model)
-        storage = simulate!(mechanism, H, record = true, solver = :mehrotra!, verbose = false)
+        storage = simulate!(mechanism, tsim, record = true, solver = :mehrotra!, verbose = false)
 
         # Set data
         Nb = length(mechanism.bodies)
@@ -45,13 +45,13 @@ function test_datamat(model::Symbol; ϵ::T = 1e-6, H::T = 0.10, Δt::T = 0.01, g
     return nothing
 end
 
-function test_sensitivity(model::Symbol; ϵ::T = 1e-6, H::T = 0.10, Δt::T = 0.01, g::T = -9.81, cf::T = 0.8,
+function test_sensitivity(model::Symbol; ϵ::T = 1e-6, tsim::T = 0.10, Δt::T = 0.01, g::T = -9.81, cf::T = 0.8,
         contact::Bool = true, verbose::Bool = false) where {T}
 
     @testset "sensitivity: $(string(model))" begin
         mechanism = getmechanism(model, Δt = Δt, g = g, cf = cf, contact = contact)
         initialize!(mechanism, model)
-        storage = simulate!(mechanism, H, record = true, solver = :mehrotra!, verbose = false)
+        storage = simulate!(mechanism, tsim, record = true, solver = :mehrotra!, verbose = false)
 
         # Set data
         Nb = length(mechanism.bodies)
@@ -72,41 +72,40 @@ function test_sensitivity(model::Symbol; ϵ::T = 1e-6, H::T = 0.10, Δt::T = 0.0
     return nothing
 end
 
-models = [
-          :atlas,
-          # :quadruped,
-          :dice,
-          # :snake,
-          :pendulum,
-          # :slider,
-          :npendulum,
-          # :nslider,
-          ]
-ϵ_solmat = Dict(
-            :atlas => 1e-8,
-            :quadruped => 1e-8,
-            :dice => 1e-8,
-            :snake => 1e-8,
-            :pendulum => 1e-8,
-            :slider => 1e-8,
-            :npendulum => 1e-8,
-            :nslider => 1e-8)
-ϵ_datamat = Dict(
-            :atlas => 1e-6,
-            :quadruped => 1e-6,
-            :dice => 1e-6,
-            :snake => 1e-6,
-            :pendulum => 1e-6,
-            :slider => 1e-6,
-            :npendulum => 1e-6,
-            :nslider => 1e-6)
+# Flying after 0.1 sec simulation
+test_solmat(:atlas, tsim = 0.10, ϵ = 1e-8)
+test_solmat(:quadruped, tsim = 0.10, ϵ = 1e-8)
+test_solmat(:dice, tsim = 0.10, ϵ = 1e-8)
+test_solmat(:snake, tsim = 0.10, ϵ = 1e-8)
+test_solmat(:slider, tsim = 0.10, ϵ = 1e-8)
+test_solmat(:pendulum, tsim = 0.10, ϵ = 1e-8)
+test_solmat(:npendulum, tsim = 0.10, ϵ = 1e-8)
+test_solmat(:nslider, tsim = 0.10, ϵ = 1e-8)
 
-for model in models
-    @show model
-    test_solmat(model, ϵ = ϵ_solmat[model])
-end
+test_datamat(:atlas, tsim = 0.10, ϵ = 1e-8)
+test_datamat(:quadruped, tsim = 0.10, ϵ = 1e-8)
+test_datamat(:dice, tsim = 0.10, ϵ = 1e-8)
+test_datamat(:snake, tsim = 0.10, ϵ = 1e-8)
+test_datamat(:slider, tsim = 0.10, ϵ = 1e-8)
+test_datamat(:pendulum, tsim = 0.10, ϵ = 1e-8)
+test_datamat(:npendulum, tsim = 0.10, ϵ = 1e-8)
+test_datamat(:nslider, tsim = 0.10, ϵ = 1e-8)
 
-for model in models
-    @show model
-    test_datamat(model, ϵ = ϵ_datamat[model])
-end
+# In contact with the ground after 0.4 sec simulation
+test_solmat(:atlas, tsim = 0.40, ϵ = 1e-8)
+test_solmat(:quadruped, tsim = 0.40, ϵ = 1e-8)
+test_solmat(:dice, tsim = 0.40, ϵ = 1e-8)
+test_solmat(:snake, tsim = 0.40, ϵ = 1e-8)
+test_solmat(:slider, tsim = 0.40, ϵ = 1e-8)
+test_solmat(:pendulum, tsim = 0.40, ϵ = 1e-8)
+test_solmat(:npendulum, tsim = 0.40, ϵ = 1e-8)
+test_solmat(:nslider, tsim = 0.40, ϵ = 1e-8)
+
+test_datamat(:atlas, tsim = 0.40, ϵ = 1e-8)
+test_datamat(:quadruped, tsim = 0.40, ϵ = 1e-8)
+test_datamat(:dice, tsim = 0.40, ϵ = 1e-8)
+test_datamat(:snake, tsim = 0.40, ϵ = 1e-8)
+test_datamat(:slider, tsim = 0.40, ϵ = 1e-8)
+test_datamat(:pendulum, tsim = 0.40, ϵ = 1e-8)
+test_datamat(:npendulum, tsim = 0.40, ϵ = 1e-8)
+test_datamat(:nslider, tsim = 0.40, ϵ = 1e-8)
