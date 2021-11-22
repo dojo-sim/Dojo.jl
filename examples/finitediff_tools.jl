b5b55060141091addae3cc3b973bf6a94e6a5932
simon-lc <simonlc@stanford.edu>
2021-11-22 14:01:46 -0800

    change setdata setsol

--- examples/finitediff_tools.jl
+++ examples/finitediff_tools.jl
@@ -5,23 +5,25 @@
 
 function unpackdata(data::AbstractVector)
     x2 = data[SVector{3,Int}(1:3)]
-    v1 = data[SVector{3,Int}(4:6)]
+    v15 = data[SVector{3,Int}(4:6)]
     q2 = data[SVector{4,Int}(7:10)]
-    ω1 = data[SVector{3,Int}(11:13)]
-    return x2, v1, q2, ω1
+    ϕ15 = data[SVector{3,Int}(11:13)]
+    return x2, v15, q2, ϕ15
 end
 
 function setdata!(mechanism::Mechanism, data::AbstractVector)
+    Δt = mechanism.Δt
     off = 0
     for body in mechanism.bodies
-        x2, v1, q2, ω1 = unpackdata(data[off+1:end]); off += 13
-        body.state.x1 = x2
-        body.state.v15 = v1
-        body.state.q1 = UnitQuaternion(q2...)
-        body.state.ϕ15 = ω1
+        x2, v15, q2, ϕ15 = unpackdata(data[off+1:end]); off += 13
+        q2 = UnitQuaternion(q2..., false)
+        body.state.x1 = getx3(x2, -v15, Δt)
+        body.state.v15 = v15
+        body.state.q1 = getq3(q2, -ϕ15, Δt)
+        body.state.ϕ15 = ϕ15
         body.state.x2[1] = x2
-        body.state.q2[1] = UnitQuaternion(q2...)
-        setsolution!(body)
+        body.state.q2[1] = q2
+        # setsolution!(body)
         body.state.F2[1] = SVector{3}([0,0,0.])
         body.state.τ2[1] = SVector{3}([0,0,0.])
     end
@@ -39,11 +41,11 @@ end
 function getdata(mechanism::Mechanism{T}) where T
     data = Vector{T}()
     for body in mechanism.bodies
-        x1 = body.state.x1
+        x2 = body.state.x2[1]
         v15 = body.state.v15
-        q1 = vector(body.state.q1)
+        q2 = vector(body.state.q2[1])
         ϕ15 = body.state.ϕ15
-        push!(data, [x1; v15; q1; ϕ15]...)
+        push!(data, [x2; v15; q2; ϕ15]...)
     end
     for eqc in mechanism.eqconstraints
         if controldim(eqc) > 0
