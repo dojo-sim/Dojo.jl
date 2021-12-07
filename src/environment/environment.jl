7c8416da19e77ba7bd58f4c74bdd3cbfb9514c3c
simon-lc <simonlc@stanford.edu>
2021-12-07 12:07:45 -0800

    starting env.jl

--- src/environment/environment.jl
+++ src/environment/environment.jl
@@ -1,30 +1,153 @@
-import Base.reset
 
-mutable struct Environement12{M}
+################################################################################
+# Reward
+################################################################################
+
+"""
+     Reward constructor. Provides a simple way to construct a reward function
+     for each envirionment: atlas, snake, dice, etc.
+"""
+function getreward(model::Symbol; kwargs...)
+    reward_fct = eval(Symbol(:getreward, model))(; kwargs...)
+    return reward_fct
+end
+
+function getrewarddice(;)
+    reward_fct(s, a) = 0.0
+    return reward_fct
+end
+
+
+################################################################################
+# Imports
+################################################################################
+
+import Base.contains
+import Base.reset
+import Base.step
+import Dojo.MeshCat.render
+
+################################################################################
+# Space
+################################################################################
+
+abstract type Space28{T,N} end
+
+mutable struct BoxSpace28{T,N} <: Space28{T,N}
+    n::Int # box dimension
+    low::AbstractVector{T} # minimum value
+    high::AbstractVector{T} # maximum value
+end
+
+function BoxSpace28(n::Int; low::AbstractVector{T} = -ones(n), high::AbstractVector{T} = ones(n)) where {T}
+    return BoxSpace28{T,n}(n, low, high)
+end
+
+function sample(s::BoxSpace28{T,N}) where {T,N}
+    return rand(T,N) .* (s.high .- s.low) .+ s.low
+end
+
+function contains(s::BoxSpace28{T,N}, v::AbstractVector{T}) where {T,N}
+    all(v .>= s.low) && all(v .<= s.high)
+end
+
+################################################################################
+# Environment
+################################################################################
+
+mutable struct Environment28{M,A,O}
     mechanism::M
+    model::Symbol
+    reward_fct::Any # reward function R(s, a) = r
+    iter::Int # current time step count
+    max_iter::Int # number of time steps per episode
+    aspace::A
+    ospace::O
+    # vis::Visualizer
+end
+
+
+function Environment28(mechanism::Mechanism, model::Symbol, reward_fct::Any; max_iter::Int = 100)
+    iter = 0
+    aspace = BoxSpace28(controldim(mechanism))
+    ospace = BoxSpace28(maxCoordDim(mechanism))
+    TYPE = [typeof(mechanism), typeof(aspace), typeof(ospace)]
+    env = Environment28{TYPE...}(mechanism, model, reward_fct, iter, max_iter, aspace, ospace)
+    return env
 end
 
 function make(model::String; kwargs...)
-    mechanism = getmechanism(Symbol(model); kwargs...)
-    env = Environement12(mechanism)
+    model = Symbol(model)
+    mechanism = getmechanism(model; kwargs...)
+    reward_fct = getreward(model)
+    env = Environment28(mechanism, model, reward_fct)
     reset(env)
     return env
 end
 
-function reset(env::Environement12{M}; kwargs...) where {M}
-    initialize!(mechanism; kwargs...)
+function reset(env::Environment28{M}; kwargs...) where {M}
+    env.iter = 0
+    initialize!(env.mechanism, Symbol(env.model); kwargs...)
+    observation = getMaxState(env.mechanism)
+    return observation
+end
+
+function render(env::Environment28{M}; kwargs...) where {M}
+
     return nothing
+end
+
+function close(env::Environment28{M}; kwargs...) where {M}
+    # visualizer stuff
+    return nothing
+end
+
+function need_to_reset(env::Environment28)
+    done = env.iter >= env.max_iter - 1
+    return done
+end
+
+function step(env::Environment28{M}, action) where {M}
+    env.iter += 1
+    mechanism = env.mechanism
+    state = getMaxState(mechanism)
+    observation = getMaxState(mechanism)
+
+    reward = env.reward_fct(state, action)
+    done = need_to_reset(env)
+    info = Dict()
+    return observation, reward, done, info
 end
 
 
 
-# script
+
+
+################################################################################
+# Script
+################################################################################
+
 env = make("dice", Δt = 0.05, g = -9.81);
-reset(env)
+reset(env, x = [0,0,0.2])
+action = nothing
+step(env, action)
 
 
-
-
+env = make("dice");
+for i = 1:20
+    observation = reset(env)
+    for t = 1:200
+        # render(env)
+        println(scn.(observation))
+        action = sample(env.aspace)
+        observation, reward, done, info = step(env, action)
+        if done
+            println("Episode finished after $(t+1) timesteps")
+            break
+        end
+    end
+end
+close(env)
 
 ################################################################################
 # Development
