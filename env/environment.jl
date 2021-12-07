ab6f7a93f01bc721a4b6075bfb77a0772ad22132
thowell <thowell@stanford.edu>
2021-12-07 13:32:29 -0800

    cleanup environment

--- env/environment.jl
+++ env/environment.jl
@@ -31,23 +31,23 @@ import Dojo.MeshCat.render
 # Space
 ################################################################################
 
-abstract type Space28{T,N} end
+abstract type Space{T,N} end
 
-mutable struct BoxSpace28{T,N} <: Space28{T,N}
+mutable struct BoxSpace{T,N} <: Space{T,N}
     n::Int # box dimension
     low::AbstractVector{T} # minimum value
     high::AbstractVector{T} # maximum value
 end
 
-function BoxSpace28(n::Int; low::AbstractVector{T} = -ones(n), high::AbstractVector{T} = ones(n)) where {T}
-    return BoxSpace28{T,n}(n, low, high)
+function BoxSpace(n::Int; low::AbstractVector{T} = -ones(n), high::AbstractVector{T} = ones(n)) where {T}
+    return BoxSpace{T,n}(n, low, high)
 end
 
-function sample(s::BoxSpace28{T,N}) where {T,N}
+function sample(s::BoxSpace{T,N}) where {T,N}
     return rand(T,N) .* (s.high .- s.low) .+ s.low
 end
 
-function contains(s::BoxSpace28{T,N}, v::AbstractVector{T}) where {T,N}
+function contains(s::BoxSpace{T,N}, v::AbstractVector{T}) where {T,N}
     all(v .>= s.low) && all(v .<= s.high)
 end
 
@@ -55,7 +55,7 @@ end
 # Environment
 ################################################################################
 
-mutable struct Environment28{M,A,O}
+mutable struct Environment{M,A,O}
     mechanism::M
     model::Symbol
     reward_fct::Any # reward function R(s, a) = r
@@ -67,12 +67,12 @@ mutable struct Environment28{M,A,O}
 end
 
 
-function Environment28(mechanism::Mechanism, model::Symbol, reward_fct::Any; max_iter::Int = 100)
+function Environment(mechanism::Mechanism, model::Symbol, reward_fct::Any; max_iter::Int = 100)
     iter = 0
-    aspace = BoxSpace28(controldim(mechanism))
-    ospace = BoxSpace28(maxCoordDim(mechanism))
+    aspace = BoxSpace(controldim(mechanism))
+    ospace = BoxSpace(maxCoordDim(mechanism))
     TYPE = [typeof(mechanism), typeof(aspace), typeof(ospace)]
-    env = Environment28{TYPE...}(mechanism, model, reward_fct, iter, max_iter, aspace, ospace)
+    env = Environment{TYPE...}(mechanism, model, reward_fct, iter, max_iter, aspace, ospace)
     return env
 end
 
@@ -80,34 +80,34 @@ function make(model::String; kwargs...)
     model = Symbol(model)
     mechanism = getmechanism(model; kwargs...)
     reward_fct = getreward(model)
-    env = Environment28(mechanism, model, reward_fct)
+    env = Environment(mechanism, model, reward_fct)
     reset(env)
     return env
 end
 
-function reset(env::Environment28{M}; kwargs...) where {M}
+function reset(env::Environment{M}; kwargs...) where {M}
     env.iter = 0
     initialize!(env.mechanism, Symbol(env.model); kwargs...)
     observation = getMaxState(env.mechanism)
     return observation
 end
 
-function render(env::Environment28{M}; kwargs...) where {M}
+function render(env::Environment{M}; kwargs...) where {M}
 
     return nothing
 end
 
-function close(env::Environment28{M}; kwargs...) where {M}
+function close(env::Environment{M}; kwargs...) where {M}
     # visualizer stuff
     return nothing
 end
 
-function need_to_reset(env::Environment28)
+function need_to_reset(env::Environment)
     done = env.iter >= env.max_iter - 1
     return done
 end
 
-function step(env::Environment28{M}, action) where {M}
+function step(env::Environment{M}, action) where {M}
     env.iter += 1
     mechanism = env.mechanism
     state = getMaxState(mechanism)
@@ -120,66 +120,3 @@ function step(env::Environment28{M}, action) where {M}
 end
 
 
-
-
-
-################################################################################
-# Script
-################################################################################
-
-env = make("dice", Δt = 0.05, g = -9.81);
-reset(env, x = [0,0,0.2])
-action = nothing
-step(env, action)
-
-
-env = make("dice");
-for i = 1:20
-    observation = reset(env)
-    for t = 1:200
-        # render(env)
-        println(scn.(observation))
-        action = sample(env.aspace)
-        observation, reward, done, info = step(env, action)
-        if done
-            println("Episode finished after $(t+1) timesteps")
-            break
-        end
-    end
-end
-close(env)
-
-################################################################################
-# Development
-################################################################################
-# Utils
-function module_dir()
-    return joinpath(@__DIR__, "..", "..")
-end
-
-# Activate package
-using Pkg
-Pkg.activate(module_dir())
-
-# Load packages
-using Plots
-using Random
-using MeshCat
-
-# Open visualizer
-vis = Visualizer()
-open(vis)
-
-# Include new files
-include(joinpath(module_dir(), "examples", "loader.jl"))
-
-mech = getmechanism(:dice, Δt = 0.01, g = -9.81, cf = 0.2, contact = true, mode=:box, conetype = :soc)
-# mech = getmechanism(:dice, Δt = 0.01, g = -9.81, cf = 0.2, contact = true, mode=:box, conetype = :linear)
-# mech = getmechanism(:dice, Δt = 0.01, g = -9.81, contact = true, mode=:box, conetype = :impact)
-Random.seed!(100)
-ω = 0.0 * (rand(3) .- 0.5) * 1
-x = [0, 0, 1.0]
-v = 1.0 * [4, 1, 0.0]
-initialize!(mech, :dice, x = x, v = v, ω = ω)
-storage = simulate!(mech, 1.3, record = true, solver = :mehrotra!, verbose = false)
-visualize(mech, storage, vis = vis)
