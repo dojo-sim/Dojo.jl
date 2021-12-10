a7da91c2ec0e6c2023bdc6fbd091ccc0748a0eaf
simon-lc <simonlc@stanford.edu>
2021-12-09 18:36:06 -0800

    adding tippetop

--- examples/dev/tippetop.jl
+++ examples/dev/tippetop.jl
@@ -107,7 +107,7 @@ end
 
 mech = getmechanism(:rectangle, Δt = 0.01, g = -0.00);
 body1 = collect(mech.bodies)[1]
-body1.J
-initialize!(mech, :rectangle, x = [0,0,1.0], q = UnitQuaternion(RotX(0.00*π)), ω = [0,5.0,0.05])
-@elapsed storage = simulate!(mech, 5.0, record = true, verbose = false, opts=InteriorPointOptions(verbose=false, btol = 1e-6))
+body1.J = Matrix(Diagonal([1,2,3.]))
+initialize!(mech, :rectangle, x = [0,0,1.0], q = UnitQuaternion(RotX(0.00*π)), ω = [0,5.0,0.01])
+@elapsed storage = simulate!(mech, 8.0, record = true, verbose = false, opts=InteriorPointOptions(verbose=false, btol = 1e-6))
 visualize(mech, storage, vis = vis)
