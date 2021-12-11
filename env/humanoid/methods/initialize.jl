519614ddf04f9daf4457340fafca3520da8f3619
thowell <thowell@stanford.edu>
2021-12-11 11:54:35 -0800

    multiple shapes per body

--- env/humanoid/methods/initialize.jl
+++ env/humanoid/methods/initialize.jl
@@ -1,18 +1,6 @@
-function gethumanoid(; Δt::T = 0.01, g::T = -9.81, cf::T = 0.8, spring = 0.0, damper = 0.0, contact::Bool = true) where {T}
-    # TODO new feature: visualize capsule instead of cylinders
-    # TODO new feature: visualize multiple shapes for a single body
+function gethumanoid(; Δt::T=0.01, g::T=-9.81, cf=0.8, spring=0.0, damper=0.0, contact::Bool=true, contact_body::Bool=false) where {T}
     path = joinpath(@__DIR__, "../deps/humanoid.urdf")
-    mech = Mechanism(path, floating=true, g = g, Δt = Δt, spring=spring, damper=damper)
-
-    # Adding springs and dampers
-    for (i,eqc) in enumerate(collect(mech.eqconstraints[2:end]))
-        eqc.isdamper = true
-        eqc.isspring = true
-        for joint in eqc.constraints
-            joint.spring = spring
-            joint.damper = damper
-        end
-    end
+    mech = Mechanism(path, floating=true, g=g, Δt=Δt, spring=spring, damper=damper)
 
     if contact
         origin = Origin{T}()
@@ -21,11 +9,11 @@ function gethumanoid(; Δt::T = 0.01, g::T = -9.81, cf::T = 0.8, spring = 0.0, d
 
         # Foot contact
         contacts = [
-            [-0.1; -0.05; 0.0],
-            [+0.1; -0.05; 0.0],
-            [-0.1; +0.05; 0.0],
-            [+0.1; +0.05; 0.0],
-            ]
+                    [-0.1; -0.05; 0.0],
+                    [+0.1; -0.05; 0.0],
+                    [-0.1; +0.05; 0.0],
+                    [+0.1; +0.05; 0.0],
+                   ]
         n = length(contacts)
         normal = [[0;0;1.0] for i = 1:n]
         cf = cf * ones(T, n)
@@ -39,12 +27,21 @@ function gethumanoid(; Δt::T = 0.01, g::T = -9.81, cf::T = 0.8, spring = 0.0, d
     return mech
 end
 
-function initializehumanoid!(mechanism::Mechanism; tran::AbstractVector{T} = [0,0,1.2],
-    rot::AbstractVector{T} = [0.1,0,0]) where {T}
-    setPosition!(mechanism,
-                geteqconstraint(mechanism, "auto_generated_floating_joint"),
-                [tran; rot])
-
+function initializehumanoid!(mechanism::Mechanism; tran=[0,0,1.5], rot=[0.1,0,0]) where T
+    setPosition!(mechanism, geteqconstraint(mechanism, "auto_generated_floating_joint"), [tran; rot])
     zeroVelocity!(mechanism)
 end
+
+vis = Visualizer()
+open(vis)
+
+humanoid = getcartpole(g=0.0);
+initializecartpole!(humanoid)
+
+storage = simulate!(humanoid, 1.0, record=true, verbose=false)
+visualize(humanoid, storage, vis=vis)
+
+
+
+
 
