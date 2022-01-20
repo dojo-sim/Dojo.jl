fb9d7089e0b323435432f8fea76a16326057ec8b
simon-lc <simonlc@stanford.edu>
2022-01-20 15:56:54 -0800

    add colors to atlas

--- examples/MuJoCoBenchmark.jl/src/cone_compare.jl
+++ examples/MuJoCoBenchmark.jl/src/cone_compare.jl
@@ -31,28 +31,19 @@ jd.qpos .= [-1.5,-0.50,0.50,1,0,0,0]
 jd.qvel .= [4.0,0.8,0,0,0,0]
 
 traj = zeros(20, 0)
-E = zeros(N)
-Plin = zeros(N)
-Pang = zeros(N)
 tcompute = 0.0
 ztraj = []
 for i = 1:N
     jd.ctrl .= 0.0
     tcompute += @elapsed mj_step(jm, jd);
     traj = hcat(traj, deepcopy(getstate(mjsim)))
-    E[i] = energy(jm, jd)
-	Plin[i] = norm(linear_momentum(jm, jd))
-	# Pang[i] = angular_momentum(jm, jd)
 	x = deepcopy(jd.qpos[1:3])
 	v = deepcopy(jd.qvel[1:3])
 	q = deepcopy(jd.qpos[4:7])
 	ω = deepcopy(jd.qvel[4:6])
 	push!(ztraj, [x; v; q; ω])
 end
-tsim / t
-plot(E)
-plot(Plin)
-# plot(Pang)
+tsim / tcompute
 
 LyceumMuJoCoViz.visualize(mjsim, trajectories=traj)
 
