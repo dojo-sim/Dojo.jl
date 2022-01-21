4fb9dcdc1de24c6e8aa59ee383b517595e10aba2
simon-lc <simonlc@stanford.edu>
2022-01-21 15:55:53 -0800

    cleaning floor utils

--- examples/dev/tippetop.jl
+++ examples/dev/tippetop.jl
@@ -1,5 +1,6 @@
 # Load packages
 using MeshCat
+using Meshing
 
 # Open visualizer
 vis = Visualizer()
@@ -18,23 +19,4 @@ visualize(mech, storage, vis=vis)
 tippytop_texture!(vis, mech)
 
 setprop!(vis["/Background"], "top_color", RGBA(0.0, 0.0, 0.0))
-
-function plot_surface!(vis::Visualizer, f::Any; xlims = [-10.0, 10.0],
-    ylims = [-10.0, 10.0], col=[1.0, 215/255, 0.0], α=1.0, n::Int=200)
-    mesh = GeometryBasics.Mesh(f,
-        HyperRectangle(Vec(xlims[1], ylims[1], -2.0), Vec(xlims[2]-xlims[1], ylims[2]-ylims[1], 4.0)),
-        Meshing.MarchingCubes(), samples=(n, n, Int(floor(n/8))))
-    setobject!(vis["surface"], mesh,
-            MeshPhongMaterial(color=RGBA{Float32}(col..., α)))
-    return nothing
-end
-
-using Meshing
 plot_surface!(vis, x -> x[3] - 0.0)
-
-mech = getmechanism(:rectangle, Δt = 0.01, g = -0.00);
-body1 = collect(mech.bodies)[1]
-body1.J = Matrix(Diagonal([1,2,3.]))
-initialize!(mech, :rectangle, x = [0,0,1.0], q = UnitQuaternion(RotX(0.00*π)), ω = [0,5.0,0.01])
-@elapsed storage = simulate!(mech, 8.0, record = true, verbose = false, opts=InteriorPointOptions(verbose=false, btol = 1e-6))
-visualize(mech, storage, vis = vis)
