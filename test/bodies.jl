d5c393fdb3ffe04f2cb903d2bd4f9504eb626eb1
thowell <thowell@stanford.edu>
2022-02-24 16:53:16 -0800

    fix bodies test run_ci

--- test/bodies.jl
+++ test/bodies.jl
@@ -1,21 +1,21 @@
 @testset "Bodies: Shapes" begin
     # shapes
-    box = Box(1.0, 1.0, 1.0)
-    cylinder = Cylinder(1.0, 1.0)
-    capsule = Capsule(1.0, 1.0)
-    shapes = Shapes([box, cylinder, capsule])
-    sphere = Sphere(1.0)
-    pyramid = Pyramid(1.0, 1.0)
-    mesh = Mesh(joinpath(@__DIR__, "../env/atlas/deps/mesh/head.obj"))
+    box = Dojo.Box(1.0, 1.0, 1.0)
+    cylinder = Dojo.Cylinder(1.0, 1.0)
+    capsule = Dojo.Capsule(1.0, 1.0)
+    shapes = Dojo.Shapes([box, cylinder, capsule])
+    sphere = Dojo.Sphere(1.0)
+    pyramid = Dojo.Pyramid(1.0, 1.0)
+    mesh = Dojo.Mesh(joinpath(@__DIR__, "../env/atlas/deps/mesh/head.obj"))
 
     # convert 
-    box_geom = convert_shape(box)
-    cylinder_geom = convert_shape(cylinder)
-    capsule_geom = convert_shape(capsule)
-    shapes_geom = convert_shape(shapes)
-    sphere_geom = convert_shape(sphere)
-    pyramid_geom = convert_shape(pyramid)
-    mesh_geom = convert_shape(mesh)
+    box_geom = Dojo.convert_shape(box)
+    cylinder_geom = Dojo.convert_shape(cylinder)
+    capsule_geom = Dojo.convert_shape(capsule)
+    shapes_geom = Dojo.convert_shape(shapes)
+    sphere_geom = Dojo.convert_shape(sphere)
+    pyramid_geom = Dojo.convert_shape(pyramid)
+    mesh_geom = Dojo.convert_shape(mesh)
     @test true
 end
 
