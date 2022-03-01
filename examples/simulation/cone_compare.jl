89aa1fdb4ab623bad209f08b90f8f9684ada5d27
simon-lc <simonlc@stanford.edu>
2022-02-28 22:14:50 -0800

    wip

--- examples/simulation/cone_compare.jl
+++ examples/simulation/cone_compare.jl
@@ -6,18 +6,17 @@
 using Dojo
 
 # ## Visualizer
-vis=visualizer()
+vis = Visualizer()
 open(vis)
-set_camera!(vis, 
-    cam_pos=[-0.01, 0.0, 90.0], 
+set_camera!(vis,
+    cam_pos=[-0.01, 0.0, 90.0],
     zoom=30)
-set_floor!(vis, 
-    x=20.0, 
-    y=20.0, 
+set_floor!(vis,
+    x=20.0,
+    y=20.0,
     color=RGBA(1.0, 1.0, 1.0, 1.0))
-set_light!(vis, 
-    ambient=0.65, 
-    fill=0.60, 
+set_light!(vis,
+    ambient=0.65,
     direction="Positive")
 
 ################################################################################
@@ -33,28 +32,28 @@ opts = SolverOptions(rtol=1.0e-6, btol=1.0e-6)
 
 # ## Linear cone
 color_lc = orange;
-mech_lc = get_mechanism(:box, 
-    timestep=timestep, 
-    gravity=gravity, 
+mech_lc = get_mechanism(:box,
+    timestep=timestep,
+    gravity=gravity,
     friction_coefficient=friction_coefficient,
-    contact_type=:linear, 
-    mode=:box, 
+    contact_type=:linear,
+    mode=:box,
     color=color_lc);
 
 # ## Simulate
-initialize!(mech_lc, :box, 
-    x=x0, 
-    q=one(UnitQuaternion), 
-    v=v0, 
+initialize!(mech_lc, :box,
+    x=x0,
+    q=one(UnitQuaternion),
+    v=v0,
     ω=ω0)
 
-storage_lc = simulate!(mech_lc, 4.0, 
-    record=true, 
+storage_lc = simulate!(mech_lc, 4.0,
+    record=true,
     opts=opts)
 
 # ## Visualize
-via, anim = visualize(mech_lc, storage_lc, 
-    vis=vis, 
+via, anim = visualize(mech_lc, storage_lc,
+    vis=vis,
     name=:lc)
 
 line_mat_lc = LineBasicMaterial(color=color_lc, linewidth=10.0)
@@ -67,28 +66,28 @@ setobject!(vis[:path_lc], MeshCat.Line(points_lc, line_mat_lc))
 
 # ## Nonlinear cone
 color_nc = cyan;
-mech_nc = get_mechanism(:box, 
-    timestep=timestep, 
-    gravity=gravity, 
+mech_nc = get_mechanism(:box,
+    timestep=timestep,
+    gravity=gravity,
     friction_coefficient=friction_coefficient,
-    contact_type=:nonlinear, 
-    mode=:box, 
+    contact_type=:nonlinear,
+    mode=:box,
     color=color_nc);
 
 # ## Simulate
-initialize!(mech_nc, :box, 
-    x=x0, 
-    q=one(UnitQuaternion), 
-    v=v0, 
+initialize!(mech_nc, :box,
+    x=x0,
+    q=one(UnitQuaternion),
+    v=v0,
     ω=ω0)
 
-storage_nc = simulate!(mech_nc, 4.0, 
-    record=true, 
+storage_nc = simulate!(mech_nc, 4.0,
+    record=true,
     opts=opts)
 
 # ## Visualize
 for (i, x) in enumerate(storage_nc.x[1])
-    storage_nc.x[1][i] += [0.0; 0.0; 0.1] 
+    storage_nc.x[1][i] += [0.0; 0.0; 0.1]
 end
 
 line_mat_nc = LineBasicMaterial(color=color_nc, linewidth=25.0)
@@ -99,34 +98,34 @@ for (i, xt) in enumerate(storage_nc.x[1])
 end
 setobject!(vis[:path_nc], MeshCat.Line(points_nc, line_mat_nc))
 
-vis, anim = visualize(mech_nc, storage_nc, 
-    vis=vis, 
-    name=:nc, 
+vis, anim = visualize(mech_nc, storage_nc,
+    vis=vis,
+    name=:nc,
     animation=anim)
 
 # ## MuJoCo cone
 color_mj = magenta;
 
-mech_mj = get_mechanism(:box, 
-    timestep=timestep, 
-    gravity=gravity, 
+mech_mj = get_mechanism(:box,
+    timestep=timestep,
+    gravity=gravity,
     friction_coefficient=friction_coefficient,
-    contact_type=:linear, 
+    contact_type=:linear,
     mode=:box, color=color_mj);
 
 # ## Load
-initialize!(mech_mj, :box, 
-    x=x0, 
-    q=one(UnitQuaternion), 
-    v=v0, 
+initialize!(mech_mj, :box,
+    x=x0,
+    q=one(UnitQuaternion),
+    v=v0,
     ω=ω0)
 file = jldopen(joinpath(@__DIR__, "../MuJoCo_benchmark/results/cone_compare.jld2"))
 storage_mj = generate_storage(mech_mj, [get_maximal_state(mech_mj), file["ztraj"]...])
 
 # ## Visualize
-vis, anim = visualize(mech_mj, storage_mj, 
-    vis=vis, 
-    name=:mj, 
+vis, anim = visualize(mech_mj, storage_mj,
+    vis=vis,
+    name=:mj,
     animation=anim)
 
 line_mat_mj = LineBasicMaterial(color=color_mj, linewidth=25.0)
