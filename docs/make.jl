d02918b21e66285e937b650340d44ee1a6265a04
thowell <thowell@stanford.edu>
2022-03-03 09:13:47 -0800

    fix docs make

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ path_assets_animations = joinpath(@__DIR__, "src/assets/animations")
 !isdir(path_assets_animations) && mkdir(path_assets_animations)
 path_animations = joinpath(@__DIR__, "../examples/animations")
 files = readdir(path_animations)
-filter!(x -> endswith(x, ".gassetsassetsassetsif"), files)
+filter!(x -> endswith(x, ".gif"), files)
 for file in files 
     cp(joinpath(path_animations, file), joinpath(path_assets_animations, file), force=true)
 end
