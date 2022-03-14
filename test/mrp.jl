f6f503754fd1967d5c3ece9bc0f0c4eb0573f514
Jan Bruedigam <jan.bruedigam@t-online.de>
2022-03-14 18:45:59 +0100

    Update mrp test

--- test/mrp.jl
+++ test/mrp.jl
@@ -9,6 +9,13 @@
     q = one(Quaternion{Float64})
     @test norm(Dojo.dmrpdq(Dojo.vector(q)) -
         ForwardDiff.jacobian(Dojo.mrp, Dojo.vector(q)), Inf) < 1.0e-5
+    @test norm(Dojo.drotation_vectordq(Dojo.vector(q)) -
+        SA[
+            0.0  2.0  0.0  0.0;
+            0.0  0.0  2.0  0.0;
+            0.0  0.0  0.0  2.0;
+        ]) < 1.0e-5
+    # TODO For zero rotation, ForwardDiff runs into a numeric singularity
     # @test norm(Dojo.drotation_vectordq(Dojo.vector(q)) -
     #     ForwardDiff.jacobian(Dojo.rotation_vector, Dojo.vector(q))) < 1.0e-5
 # end
