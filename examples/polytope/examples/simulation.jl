54aecd7cf2583cb40ec07a79c253ac6a2e997ac4
simon-lc <simonlc@stanford.edu>
2022-03-18 15:35:27 -0700

    nerf to nerf works

--- examples/polytope/examples/simulation.jl
+++ examples/polytope/examples/simulation.jl
@@ -49,18 +49,17 @@ set_floor!(vis, color=RGBA(0.4,0.4,0.4,0.8), z=0.5, origin=halfspace.origin, nor
 set_background!(vis)
 
 timestep = 0.01
-H = Int(floor(0.6/timestep))
+H = Int(floor(1.0/timestep))
 mass = soft.mass
 inertia = soft.inertia
 gravity = [0,0,-9.81]
 
-x2 = [0,0,1.3]
-v15 = 0*[0,2,3.0]
-q2 = rand(4)
-q2 /= norm(q2)
-q2 = Quaternion(q2...)
-q2 = Quaternion(1,0,0,0.0)
-ϕ15 = 0*[3.5,0,0.0]
+x2 = [0,-1.5,0.5]
+v15 = [0,5,3.0]
+# q2 = rand(4)
+# q2 /= norm(q2)
+# q2 = Quaternion(q2...)
+ϕ15 = [3.5,0,0.0]
 z0 = [x2; v15; Dojo.vector(q2); ϕ15]
 
 ψ, impact_normal, barycenter = collision(sphere, soft)
@@ -68,17 +67,17 @@ z0 = [x2; v15; Dojo.vector(q2); ϕ15]
 
 soft.options = ColliderOptions19(
     impact_damper=1e7,
-    impact_spring=3e6,
+    impact_spring=1e7,
     sliding_drag=0.0,
-    sliding_friction=0.30,
+    sliding_friction=0.3,
     rolling_drag=0.05,
     rolling_friction=0.01,
 )
 # @time Z0, Φ0 = implicit_simulation(halfspace, soft, timestep, H, z0)
 # @time Z0, Φ0 = implicit_simulation(sphere, soft, timestep, H, z0)
 soft1 = deepcopy(soft)
-soft1.x = [0,0,0.0]
-soft1.q = Quaternion(0,1,0,0.,true)
+soft1.x = [0,0,-0.08]
+soft1.q = Quaternion(1,0,0,0.,true)
 @time Z0, Φ0 = implicit_simulation(soft1, soft, timestep, H, z0)
 plt = plot(layout=(3,1), xlabel="time")
 plot!(plt[1,1], [i*timestep for i=0:H], [z[1] for z in Z0], linewidth=3.0, label="x", color=:blue)
@@ -95,7 +94,7 @@ collision(soft1, soft)
 
 build_mesh!(tight_mesh, vis, name=:tight_mesh, color=RGBA(1,1,1,1.0))
 build_mesh!(mesh, vis, name=:mesh, color=RGBA(1,1,1,0.3))
-build_mesh!(tight_mesh, vis, name=:tight_mesh_1, color=RGBA(0,0,0,1.0))
+build_mesh!(tight_mesh, vis, name=:tight_mesh_1, color=RGBA(0.2,0.2,0.2,1.0))
 build_mesh!(mesh, vis, name=:mesh_1, color=RGBA(1,1,1,0.3))
 build_sphere!(sphere, vis, name=:sphere, color=RGBA(0.4,0.4,0.4,1.0))
 animation = MeshCat.Animation(Int(floor(1/timestep)))
@@ -112,4 +111,4 @@ setanimation!(vis, animation)
 # open(joinpath(results_dir, "bunny_hull.html"), "w") do file
 #     write(file, static_html(vis))
 # end
-Dojo.convert_frames_to_video_and_gif("bunny_on_top_of_bunny")
+# Dojo.convert_frames_to_video_and_gif("bunny_vs_bunny")
