65382416421978c7b138ef93e3f1f8663b2f24c6
taylor <thowell@stanford.edu>
2022-03-21 17:56:22 -0700

    newton's cradle in progress

--- examples/simulation/newtons_cradle.jl
+++ examples/simulation/newtons_cradle.jl
@@ -1,57 +1,133 @@
-timestep=0.1
+timestep=1.0e-4
 gravity=-9.81
 
 T=Float64
 
 # Parameters
 height = 1.0
-num_rods = 3
+num_balls = 2
 rod_separation = 0.1
 
 mass_connector = 1.0 
-length_connector = (num_rods - 1) * rod_separation
+length_connector = (num_balls - 1) * rod_separation
 radius_connector = 0.025
 
-mass_rod = 0.1 
+mass_rod = 0.001
 length_rod = 0.5
 radius_rod = 0.01
+
+mass_ball = 1.0
+radius_ball = 0.5 * rod_separation
 
 # Bodies
 origin = Origin{T}()
 connector = Capsule(radius_connector, length_connector, mass_connector, 
-    axis_offset=Quaternion(0.707107, 0.0, 0.707107, 0.0),
+    axis_offset=Quaternion(0.707107, 0.707107, 0.0,  0.0),
     name=:connector)
 
-rods = [Capsule(radius_rod, length_rod, mass_rod, name=Symbol("rod_$i")) for i = 1:num_rods]
+rods = [Capsule(radius_rod, length_rod, mass_rod, 
+    name=Symbol("rod_$i")) for i = 1:num_balls]
+balls = [Sphere(radius_ball, mass_ball, 
+    name=Symbol("ball_$i")) for i = 1:num_balls]
 
-# Constraints
-joint_axis = [0.0; 1.0; 0.0]
+# Joints
+joint_axis = [1.0; 0.0; 0.0]
+damper = 0.0
+joint_world_connector = JointConstraint(Fixed(origin, connector, 
+    parent_vertex=[0.0; 0.0; height]))
 
-joint_world_connector = JointConstraint(Fixed(origin, connector, parent_vertex=[0.0; 0.0; height]))
+joint_connector_rods = [JointConstraint(Revolute(connector, rod, joint_axis; 
+    damper=damper,
+    parent_vertex=[0.0; (i - 1) * rod_separation - 0.5 * rod_separation * (num_balls - 1);  0.0], 
+    child_vertex=[0.0; 0.0; 0.5 * length_rod]), 
+    name=Symbol("connector_rod_$i")) for (i, rod) in enumerate(rods)]
 
-joint_connector_rods = [JointConstraint(Revolute(connector, rod, joint_axis; parent_vertex=[(i - 1) * rod_separation - 0.5 * rod_separation * (num_rods - 1); 0.0; 0.0], child_vertex=[0.0; 0.0; 0.5 * length_rod]), name=Symbol("connector_rod_$i")) for (i, rod) in enumerate(rods)]
+# i = 1
+# fixed_joint = JointConstraint(Fixed(connector, rods[i]; 
+#         parent_vertex=[0.0; (i - 1) * rod_separation - 0.5 * rod_separation * (num_balls - 1);  0.0], 
+#         child_vertex=[0.0; 0.0; 0.5 * length_rod]), 
+#         name=Symbol("connector_rod_$i"))
 
-bodies = [connector, rods...]
-joints = [joint_world_connector, joint_connector_rods...]
+# joint_connector_rods = [fixed_joint, joint_connector_rods[2]]
 
-mech = Mechanism(origin, bodies, joints,
+
+joint_rods_balls = [JointConstraint(Fixed(rods[i], balls[i], 
+    parent_vertex=[0.0; 0.0; -0.5 * length_rod], 
+    child_vertex=[0.0; 0.0; 0.0])) for i = 1:num_balls]
+
+# Contacts
+friction_coefficient = 0.5 
+
+collision = SphereSphereCollision{Float64,2,3,6}(
+    [0.0; 0.0; 0.0],
+    [0.0; 0.0; 0.0],
+    radius_ball, 
+    radius_ball)
+
+friction_parameterization = SA{Float64}[
+        1.0  0.0
+        0.0  1.0
+]
+
+body_body = NonlinearContact{Float64,8}(friction_coefficient, friction_parameterization, collision)
+
+# collision = SphereSphereCollision{Float64,0,3,0}(
+#     [0.0; 0.0; 0.0],
+#     [0.0; 0.0; 0.0],
+#     radius_ball, 
+#     radius_ball)
+
+# friction_parameterization = szeros(Float64, 0, 2)
+
+# body_body = ImpactContact{Float64,8}(friction_parameterization, collision)
+
+bodies = [connector, rods..., balls...]
+joints = [joint_world_connector, joint_connector_rods..., joint_rods_balls...]
+contacts = [ContactConstraint((body_body, bodies[i + 1 + num_balls].id, bodies[i + 1 + num_balls + 1].id), name=Symbol("body_body_$i")) for i = 1:(num_balls-1)]
+
+mech = Mechanism(origin, bodies, joints, contacts,
     gravity=gravity,
     timestep=timestep)
 
-set_minimal_state!(mech, [-0.1; 0.0; 0.0; 0.0; 0.0; 0.0])
+minimal_dimension(mech)
+set_minimal_state!(mech, [0.25; 0.0; 0.0; 0.0; 0.0; 0.0])
+
+@show adjacency_matrix(mech.joints, mech.bodies, mech.contacts) 
+
+distance(mech.contacts[1].model.collision, 
+    mech.bodies[1 + num_balls + 1].state.x2, mech.bodies[1 + num_balls + 1].state.q2,
+    mech.bodies[1 + num_balls + 2].state.x2, mech.bodies[1 + num_balls + 2].state.q2,)
+
+contact_point(:parent, mech.contacts[1].model.collision, 
+    mech.bodies[1 + num_balls + 1].state.x2, mech.bodies[1 + num_balls + 1].state.q2,
+    mech.bodies[1 + num_balls + 2].state.x2, mech.bodies[1 + num_balls + 2].state.q2,)
+
+contact_point(:child, mech.contacts[1].model.collision, 
+    mech.bodies[1 + num_balls + 1].state.x2, mech.bodies[1 + num_balls + 1].state.q2,
+    mech.bodies[1 + num_balls + 2].state.x2, mech.bodies[1 + num_balls + 2].state.q2,)
+
+contact_normal(mech.contacts[1].model.collision, 
+    mech.bodies[1 + num_balls + 1].state.x2, mech.bodies[1 + num_balls + 1].state.q2,
+    mech.bodies[1 + num_balls + 2].state.x2, mech.bodies[1 + num_balls + 2].state.q2,)
+
+contact_tangent(mech.contacts[1].model.collision, 
+    mech.bodies[1 + num_balls + 1].state.x2, mech.bodies[1 + num_balls + 1].state.q2,
+    mech.bodies[1 + num_balls + 2].state.x2, mech.bodies[1 + num_balls + 2].state.q2,)
 
 storage = simulate!(mech, 1.0, 
     record=true, 
     opts=SolverOptions(rtol=1.0e-6, btol=1.0e-6, verbose=true))
 
+vis = Visualizer()
+render(vis)
 visualize(mech, storage, 
-    vis=vis, show_contact=true)
+    vis=vis)
 
 # ## Visualize
 # vis = Visualizer()
 # render(vis)
-# build_robot(mech, vis=vis)
-# set_robot(vis, mech, get_maximal_state(mech))
+build_robot(mech, vis=vis)
+set_robot(vis, mech, get_maximal_state(mech), show_contact=false)
 
 
 
--- src/contacts/collisions/collision.jl
+++ src/contacts/collisions/collision.jl
@@ -76,8 +76,17 @@ function ∂contact_normal_transpose∂q(jacobian::Symbol, collision::Collision,
 
     Q = ∂normalize∂x(dir) * (∂contact_point∂q(:parent, jacobian, collision, xp, qp, xc, qc) - ∂contact_point∂q(:child, jacobian, collision, xp, qp, xc, qc))
 
+    # Jacobians
+    # if jacobian == :parent 
+    #     FD = FiniteDiff.finite_difference_jacobian(q -> contact_normal(collision, xp, Quaternion(q..., false), xc, qc)', vector(qp))
+    # elseif jacobian == :child 
+    #     FD = FiniteDiff.finite_difference_jacobian(q -> contact_normal(collision, xp, qp, xc, Quaternion(q..., false))', vector(qc))
+    # end
+
     # distance 
     dis = distance(collision, xp, qp, xc, qc)
+
+    # @assert norm((dis >= 0.0 ? 1.0 : -1.0) * Q - FD, Inf) < 1.0e-2
 
     # normalized direction
     if dis >= 0.0
@@ -95,6 +104,7 @@ function contact_tangent_one(collision::Collision, xp, qp, xc, qc)
     w = [1.0; 0.0; 0.0]
     t1 = skew(w) * n' # tangent
     if !(norm(t1) > 1.0e-6)
+        # @warn "edge case!"
         w = [0.0; 1.0; 0.0]
         t1 = skew(w) * n' # tangent
     end
@@ -146,6 +156,14 @@ function ∂contact_tangent_one_transpose∂x(jacobian::Symbol, collision::Colli
     end
    
     ∂t1∂x = skew(w) * ∂contact_normal_transpose∂x(jacobian, collision, xp, qp, xc, qc)
+    
+    # if jacobian == :parent 
+    #     FD = FiniteDiff.finite_difference_jacobian(x -> contact_tangent(collision, x, qp, xc, qc)[1, :]', xp)
+    # elseif jacobian == :child 
+    #     FD = FiniteDiff.finite_difference_jacobian(x -> contact_tangent(collision, xp, qp, x, qc)[1, :]', xc)
+    # end 
+
+    # @assert norm(FD - ∂t1∂x, Inf) < 1.0e-2
 
     return ∂t1∂x
 end
@@ -157,6 +175,14 @@ function ∂contact_tangent_two_transpose∂x(jacobian::Symbol, collision::Colli
    
     ∂t1∂x = ∂contact_tangent_one_transpose∂x(jacobian, collision, xp, qp, xc, qc)
     ∂t2∂x = skew(t1') * ∂contact_normal_transpose∂x(jacobian, collision, xp, qp, xc, qc) + ∂skew∂p(n') * ∂t1∂x
+    
+    # if jacobian == :parent 
+    #     FD = FiniteDiff.finite_difference_jacobian(x -> contact_tangent(collision, x, qp, xc, qc)[2, :]', xp)
+    # elseif jacobian == :child 
+    #     FD = FiniteDiff.finite_difference_jacobian(x -> contact_tangent(collision, xp, qp, x, qc)[2, :]', xc)
+    # end
+
+    # @assert norm(FD - ∂t2∂x, Inf) < 1.0e-2
 
     return ∂t2∂x
 end
@@ -174,6 +200,15 @@ function ∂contact_tangent_one_transpose∂q(jacobian::Symbol, collision::Colli
     end
    
     ∂t1∂q = skew(t1) * ∂contact_normal_transpose∂q(jacobian, collision, xp, qp, xc, qc)
+    
+    # if jacobian == :parent 
+    #     FD = FiniteDiff.finite_difference_jacobian(q -> contact_tangent(collision, xp, Quaternion(q..., false), xc, qc)[1, :]', vector(qp))
+    # elseif jacobian == :child 
+    #     FD = FiniteDiff.finite_difference_jacobian(q -> contact_tangent(collision, xp, qp, xc, Quaternion(q..., false))[1, :]', vector(qc))
+    # end
+
+    # return FD
+    # @assert norm(FD - ∂t1∂q, Inf) < 1.0e-2
 
     return ∂t1∂q
 end
@@ -194,6 +229,14 @@ function ∂contact_tangent_two_transpose∂q(jacobian::Symbol, collision::Colli
     ∂t1∂q = skew(w) * ∂contact_normal_transpose∂q(jacobian, collision, xp, qp, xc, qc)
 
     ∂t2∂q = skew(t1) * ∂contact_normal_transpose∂q(jacobian, collision, xp, qp, xc, qc) + ∂skew∂p(n') * ∂t1∂q
+    
+    # if jacobian == :parent 
+    #     FD = FiniteDiff.finite_difference_jacobian(q -> contact_tangent(collision, xp, Quaternion(q..., false), xc, qc)[2, :]', vector(qp))
+    # elseif jacobian == :child 
+    #     FD = FiniteDiff.finite_difference_jacobian(q -> contact_tangent(collision, xp, qp, xc, Quaternion(q..., false))[2, :]', vector(qc))
+    # end
+
+    # @assert norm(FD - ∂t2∂q, Inf) < 1.0e-2
 
     return ∂t2∂q
 end
