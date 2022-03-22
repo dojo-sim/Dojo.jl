b4f554190ba248f1a87e34b0e2e15ca5812bedb5
thowell <thowell@stanford.edu>
2022-03-21 22:21:07 -0700

    trajopt examples cleanup

--- examples/trajectory_optimization/pendulum_max.jl
+++ examples/trajectory_optimization/pendulum_max.jl
@@ -10,27 +10,29 @@ using LinearAlgebra
 # ## system
 timestep = 0.05
 gravity=-9.81
-max_torque = 200.0
-max_speed = 8.0
+max_torque = 1.0e6
+max_speed = 1.0e6
 env = get_environment(:pendulum, 
     representation=:maximal, 
     max_speed=max_speed, 
     max_torque=max_torque,
-    damper=1.0,
+    damper=0.0,
     timestep=timestep,
-    gravity=gravity,
-    vis=vis);
+    gravity=gravity)
+
+# ## visualizer 
+open(env.vis)
 
 # ## dimensions
 n = env.num_states
 m = env.num_inputs
 
 # ## states
-z1 = pendulum_nominal_max()
-zT = pendulum_goal_max() 
+z1 = Dojo.pendulum_nominal_max()
+zT = Dojo.pendulum_goal_max() 
 
 # ## horizon
-T = 51
+T = 26
 
 # ## model
 dyn = IterativeLQR.Dynamics(
@@ -42,23 +44,23 @@ dyn = IterativeLQR.Dynamics(
 model = [dyn for t = 1:T-1]
 
 # ## rollout
-ū = [1.0 * randn(m) for t = 1:T-1]
+ū = [10.0 * randn(m) for t = 1:T-1]
 x̄ = IterativeLQR.rollout(model, z1, ū)
 visualize(env, x̄) 
 
 # ## objective
-ot = (x, u, w) -> transpose(x - zT) * Diagonal(1.0e-1 * ones(n)) * (x - zT) + transpose(u) * Diagonal(1.0e-2 * ones(m)) * u
-oT = (x, u, w) -> transpose(x - zT) * Diagonal(1.0e-1 * ones(n)) * (x - zT)
+ot = (x, u, w) -> transpose(x - zT) * Diagonal(1.0 * ones(n)) * (x - zT) + transpose(u) * Diagonal(1.0 * ones(m)) * u
+oT = (x, u, w) -> transpose(x - zT) * Diagonal(1.0 * ones(n)) * (x - zT)
 
-ct = Cost(ot, n, m)
-cT = Cost(oT, n, 0)
+ct = IterativeLQR.Cost(ot, n, m)
+cT = IterativeLQR.Cost(oT, n, 0)
 obj = [[ct for t = 1:T-1]..., cT]
 
 # ## constraints 
 function ctrl_lmt(x, u, w) 
     [
-     -max_torque - u[1]; 
-     u[1] - max_torque;
+    #  -max_torque - u[1]; 
+    #  u[1] - max_torque;
     ]
 end 
 
@@ -66,8 +68,9 @@ function goal(x, u, w)
     x - zT
 end
 
-cont = IterativeLQR.Constraint(ctrl_lmt, n, m, 
-    idx_ineq=collect(1:2))
+# cont = IterativeLQR.Constraint(ctrl_lmt, n, m, 
+#     idx_ineq=collect(1:2))
+cont = IterativeLQR.Constraint()
 conT = IterativeLQR.Constraint(goal, n, 0)
 cons = [[cont for t = 1:T-1]..., conT]
 
@@ -80,7 +83,7 @@ s = IterativeLQR.solver(model, obj, cons,
         obj_tol=1.0e-3,
         grad_tol=1.0e-3,
         max_iter=100,
-        max_al_iter=5,
+        max_al_iter=10,
         ρ_init=1.0,
         ρ_scale=10.0))
 IterativeLQR.initialize_controls!(s, ū)
@@ -91,6 +94,7 @@ IterativeLQR.solve!(s)
 
 # ## solution
 x_sol, u_sol = IterativeLQR.get_trajectory(s)
+
 visualize(env, x_sol)
 
 
