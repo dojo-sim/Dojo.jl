36e20f3a232902e2520c0b88361d94fe5cff1c9f
taylor <thowell@stanford.edu>
2022-04-10 15:11:04 -0700

    clipping verbosity

--- src/solver/line_search.jl
+++ src/solver/line_search.jl
@@ -12,7 +12,8 @@ function line_search!(mechanism::Mechanism, α, rvio, bvio, opts)
         end
         for body in mechanism.bodies
             ϕmax = 3.9 / mechanism.timestep^2
-            candidate_step!(α, mechanism, body, get_entry(system, body.id), scale, ϕmax = ϕmax)
+            candidate_step!(α, mechanism, body, get_entry(system, body.id), scale, 
+                ϕmax=ϕmax, verbose=opts.verbose)
             if dot(body.state.ϕsol[2], body.state.ϕsol[2]) > 3.91 / mechanism.timestep^2
                 error("Excessive angular velocity. Body-ID: $(string(body.name)) " * string(body.id) * ", ω: " * string(body.state.ϕsol[2]) * ".")
             end
@@ -135,13 +136,14 @@ function second_order_cone_step_length(λ::AbstractVector{T}, Δ::AbstractVector
     return α
 end
 
-function candidate_step!(α, mechanism::Mechanism, body::Body, vector_entry::Entry, scale; ϕmax = Inf)
+function candidate_step!(α, mechanism::Mechanism, body::Body, vector_entry::Entry, scale; 
+    ϕmax = Inf, verbose=true)
     body.state.vsol[2] = body.state.vsol[1] + 1 / (2^scale) * α * vector_entry.value[SA[1; 2; 3]]
     body.state.ϕsol[2] = body.state.ϕsol[1] + 1 / (2^scale) * α * vector_entry.value[SA[4; 5; 6]]
     ϕ = body.state.ϕsol[2]
     ϕdot = dot(ϕ, ϕ)
     if ϕdot > ϕmax
-        println("clipping ", scale, scn((ϕdot - ϕmax) / ϕmax), " ", scn(ϕdot), " ", scn(ϕmax), " ", body.name)
+        verbose && println("clipping ", scale, scn((ϕdot - ϕmax) / ϕmax), " ", scn(ϕdot), " ", scn(ϕmax), " ", body.name)
         body.state.ϕsol[2] *= ϕmax / ϕdot # this is overkill, but works better than sqrt(ϕmax/ϕdot)
     end
     return
