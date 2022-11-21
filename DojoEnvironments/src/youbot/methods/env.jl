8efb77940b33fe37fd2aaca5df061c4318daefec
Jan Bruedigam <jan.bruedigam@t-online.de>
2022-11-21 16:39:50 +0100

    Update youbot URDFs

--- DojoEnvironments/src/youbot/methods/env.jl
+++ DojoEnvironments/src/youbot/methods/env.jl
@@ -9,25 +9,23 @@ using Dojo
 
 
 # path = "DojoEnvironments/src/youbot/deps/youbot_arm_only.urdf"
-path = "DojoEnvironments/src/youbot/deps/youbot_arm_only_fixed_gripper.urdf"
+# path = "DojoEnvironments/src/youbot/deps/youbot_arm_only_fixed_gripper.urdf"
 # path = "DojoEnvironments/src/youbot/deps/youbot_base_only.urdf"
-# path = "DojoEnvironments/src/youbot/deps/youbot.urdf"
+path = "DojoEnvironments/src/youbot/deps/youbot.urdf"
 # path = "DojoEnvironments/src/youbot/deps/youbot_dual_arm.urdf"
 # path = "DojoEnvironments/src/youbot/deps/youbot_with_cam3d.urdf"
 mech = Mechanism(path; gravity = -9.81, timestep = 0.01)
 
-joint1 = get_joint(mech,:arm_joint_1)
-joint2 = get_joint(mech,:arm_joint_2)
-joint3 = get_joint(mech,:arm_joint_3)
-joint4 = get_joint(mech,:arm_joint_4)
-joint5 = get_joint(mech,:arm_joint_5)
+joint1 = get_joint(mech,:base_footprint_joint)
 
-set_minimal_coordinates!(mech,joint1,[0])
-set_minimal_coordinates!(mech,joint2,[0])
-set_minimal_coordinates!(mech,joint3,[0])
-set_minimal_coordinates!(mech,joint4,[0])
-set_minimal_coordinates!(mech,joint5,[0])
+set_minimal_coordinates!(mech,joint1,[0;0;1.5])
 
-storage = simulate!(mech, 5.0, record = true)
-visualize(mech, storage)
+function controller!(mechanism,k)
+    # set_input!(joint1, [0.1;0.1;0.4])
+end
+
+storage = simulate!(mech, 5.0, controller!, record = true)
+visualize(mech, storage)[1]
+
+
  
