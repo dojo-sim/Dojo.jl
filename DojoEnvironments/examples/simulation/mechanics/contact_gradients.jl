869520556a6f0bf2f5eb7b25b8e7647512feec52
Jan Bruedigam <jan.bruedigam@t-online.de>
2023-04-08 13:23:59 +0200

    Update gradient and mechanics examples

---
+++ DojoEnvironments/examples/simulation/mechanics/contact_gradients.jl
@@ -0,0 +1,75 @@
+# using Pkg
+# Pkg.activate(joinpath(@__DIR__, ".."))
+# Pkg.instantiate()
+
+# ## Setup
+using Dojo 
+using DojoEnvironments
+using Plots
+using LinearAlgebra
+
+function get_gradients(mechanism::Mechanism, F, mode; 
+    rtol=1e-10, btol=1e-10, 
+    undercut=1.0, no_progress_undercut=1.0)
+
+    (mode == :friction) && (idx = 1)
+    (mode == :impact) && (idx = 2)
+    opts = SolverOptions(;
+        rtol, btol, undercut, no_progress_undercut)
+
+    initialize!(mechanism, :block2d, 
+        position=[0, 0], velocity=[0, 0], 
+        orientation=0, angular_velocity=0)
+
+    x0 = get_minimal_state(mechanism)
+    u0 = zeros(3)
+    u0[idx] += F 
+
+    _, ∇u = get_minimal_gradients!(mechanism, x0, u0; opts)
+
+    Dojo.step_minimal_coordinates!(mechanism, x0, u0; opts)
+
+    z1 = Dojo.get_next_state(mechanism)
+    x1 = maximal_to_minimal(mechanism, z1)
+
+    ∂x∂F = ∇u[idx,idx]
+    Δx = x1[idx] - x0[idx]
+
+    return Δx, ∂x∂F
+end
+
+# ## scale system for nice plots
+mech = get_mechanism(:block2d; 
+    timestep=0.1, gravity=-1.0, 
+    friction_coefficient=1.0, contact=true)
+
+# ## inputs
+F_ref = 0.5:0.01:1.5
+tolerances = [1e-4, 1e-6, 1e-8, 1e-10]
+
+# ## Gradients
+gradient_plot = plot(layout=(2,2), size=(800,600), legend=:topleft)
+
+# ## Impact
+for btol in tolerances
+    gradients = [get_gradients(mech, F, :impact; btol) for F = F_ref]
+    x = [r[1] for r in gradients]
+    ∇x = [r[2] for r in gradients] 
+    plot!(gradient_plot[1,1], F_ref,  x, xlabel="Fz", ylabel="z", linewidth=3.0, linestyle=:dot,
+        label="Dojo btol ="*Dojo.scn(btol))
+    plot!(gradient_plot[2,1], F_ref, ∇x, xlabel="Fz", ylabel="∇z", linewidth=3.0, linestyle=:dot,
+        label="Dojo btol ="*Dojo.scn(btol)[2:end])
+    display(gradient_plot)
+end
+
+# ## Friction
+for btol in tolerances
+    gradients = [get_gradients(mech, F, :friction; btol) for F = F_ref]
+    x = [r[1] for r in gradients]
+    ∇x = [r[2] for r in gradients]
+    plot!(gradient_plot[1,2], F_ref,  x, xlabel="Fx", ylabel="x", linewidth=3.0, linestyle=:dot,
+        label="Dojo btol ="*Dojo.scn(btol))
+    plot!(gradient_plot[2,2], F_ref, ∇x, xlabel="Fx", ylabel="∇x", linewidth=3.0, linestyle=:dot,
+        label="Dojo btol ="*Dojo.scn(btol)[2:end])
+    display(gradient_plot)
+end
--- DojoEnvironments/examples/simulation/mechanics/energy_momentum_conservation.jl
+++ DojoEnvironments/examples/simulation/mechanics/energy_momentum_conservation.jl
@@ -47,33 +47,36 @@ end
 # ## Plots
 color = [:magenta, :orange, :cyan];
 mech, _ = simulation_loop(0.01, 0.01)
+conservation_plot = plot(layout=(3,1), size=(800,500), legend=:topleft)
 
 # ## Energy
 energies = [Dojo.mechanical_energy(mech, storage)[2:end] for storage in storages]
-energy_plot = plot(xlabel="time (s)", ylabel="energy drift (J)");
+energy_plot = plot(xlabel="time (s)", ylabel="energy drift");
 for (i, energy) in enumerate(energies)
     time = [step * timesteps[i] for step = 1:length(energy)]
-    energy_plot = plot!(time, energy .- energy[1], color=color[i], width=2.0, label="h = $(timesteps[i])")
+    plot!(conservation_plot[1,1], time, energy .- energy[1]; width=2.0, color=color[i],
+        xlabel="time (s)", ylabel="energy drift", label="h = $(timesteps[i])")
+    display(conservation_plot)
 end
-display(energy_plot)
 
 # ## Momentum
 momenta = [Dojo.momentum(mech, storage)[1:end] for storage in storages]
 
 # ## linear
 momenta_linear = [[Vector(momentum_i)[1:3] for momentum_i in momentum] for momentum in momenta]
-momentum_linear_plot = plot(ylims=(-1.0e-10, 1.0e-10), xlabel="time (s)", ylabel="linear momentum drift");
 for (i, momentum) in enumerate(momenta_linear)
     time = [step * timesteps[i] for step = 1:length(momentum)]
-    momentum_linear_plot = plot!(time, hcat([momentum_i - momentum[1] for momentum_i in momentum]...)', color=color[i], label=["h = $(timesteps[i])" "" ""])
+    plot!(conservation_plot[2,1], time, hcat([momentum_i - momentum[1] for momentum_i in momentum]...)'; width=2.0, color=color[i],
+        xlabel="time (s)", ylabel="linear mom. drift", label=["h = $(timesteps[i])" "" ""])
+    display(conservation_plot)
 end
-display(momentum_linear_plot)
 
 # ## angular 
 momenta_angular = [[Vector(momentum_i)[4:6] for momentum_i in momentum] for momentum in momenta]
 momentum_angular_plot = plot(ylims=(-1.0e-10, 1.0e-10), xlabel="time (s)", ylabel="angular momentum drift");
 for (i, momentum) in enumerate(momenta_angular)
     time = [j * timesteps[i] for j in 1:length(momentum)]
-    momentum_angular_plot = plot!(time, hcat([momentum_i - momentum[1] for momentum_i in momentum]...)', color=color[i], label=["h = $(timesteps[i])" "" ""])
+    plot!(conservation_plot[3,1], time, hcat([momentum_i - momentum[1] for momentum_i in momentum]...)'; width=2.0, color=color[i],
+        xlabel="time (s)", ylabel="angular mom. drift", label=["h = $(timesteps[i])" "" ""])
+    display(conservation_plot)
 end
-display(momentum_angular_plot)
