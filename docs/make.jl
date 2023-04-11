72d1690409d7c7a6827749f11e1b9126d18d2b8f
Jan Bruedigam <jan.bruedigam@t-online.de>
2023-04-11 11:24:10 +0200

    Upload docs animations

--- docs/make.jl
+++ docs/make.jl
@@ -2,16 +2,6 @@ push!(LOAD_PATH, "../src/")
 
 using Documenter, Dojo
 
-# copy animations to docs/src/assets/animations
-# path_assets_animations = joinpath(@__DIR__, "src/assets/animations")
-# !isdir(path_assets_animations) && mkdir(path_assets_animations)
-# path_animations = joinpath(@__DIR__, "../examples/animations")
-# files = readdir(path_animations)
-# filter!(x -> endswith(x, ".gif"), files)
-# for file in files 
-#     cp(joinpath(path_animations, file), joinpath(path_assets_animations, file), force=true)
-# end
-
 makedocs(
     modules = [Dojo],
     format = Documenter.HTML(prettyurls=false),
